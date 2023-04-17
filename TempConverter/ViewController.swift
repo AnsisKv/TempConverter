1d7de67aca8d64a75c27e0513a65d73f4f6b33cf
AnsisKv <ansiskvelde@icloud.com>
2023-04-17 20:43:34 +0300

    Initial Commit

---
+++ TempConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TempConverter
+//
+//  Created by kvelde.niklavs.ansis on 17/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
