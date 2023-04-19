e26222059ce518d93581ab44085572a36c35cdce
AnsisKv <ansiskvelde@icloud.com>
2023-04-19 13:19:00 +0300

    TempConverter

--- TempConverter/ViewController.swift
+++ TempConverter/ViewController.swift
@@ -9,11 +9,57 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var tempSegmentController: UISegmentedControl!
+    
+    @IBOutlet weak var tempSlider: UISlider!{
+        didSet{
+            tempSlider.maximumValue = 100
+            tempSlider.minimumValue = 0
+            tempSlider.value = 0
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        celsiusLabel.text = "32 Fº"
     }
-
-
+    
+    @IBAction func sliderValueChanged(_ sender: Any) {
+//        print("tempSlider:", tempSlider.value)
+        updateResultLabelValue(value: tempSlider.value)
+    }
+    
+    
+    @IBAction func segmentValueChange(_ sender: Any) {
+        updateResultLabelValue(value: tempSlider.value)
+    }
+    
+    func updateResultLabelValue(value: Float){
+    print(value)
+        
+        let celsiusTemp = Int(value)
+        celsiusLabel.text = "\(celsiusTemp) Cº"
+    
+        var convertedTempString = ""
+        switch tempSegmentController.selectedSegmentIndex{
+        case 0:
+            let fahrenheitTempString = String(format: "%.2F", convertTempFrom(celsius: celsiusTemp).fahrenheit)
+            convertedTempString = fahrenheitTempString + " Fº"
+        default:
+            let kelvinTempString = String(format: "%.2F", convertTempFrom(celsius: celsiusTemp).kelvin)
+            convertedTempString = kelvinTempString + " Kº"
+        }
+        resultLabel.text = convertedTempString
+    }
+    
+    
+    func convertTempFrom(celsius: Int) -> (fahrenheit: Double, kelvin: Double){
+        let fahrenheit = (Double(celsius) * 9 / 5) + 32
+        let kelvin = Double(celsius) + 273.15
+        return(fahrenheit,kelvin)
+    }
+    
 }
 
